ce8f0a796e86cd4914ce1957a5a57401c5076845
ddock4you <ddock4you@gmail.com>
2020-03-04 22:08:00 +0900

    - youtube api를 통해 데이터 받아올 수 있는지 테스트

--- src/Routes/NowList/index.js
+++ src/Routes/NowList/index.js
@@ -5,6 +5,14 @@ import axios from "axios";
 const NowList = () => {
     const javasc = "javascript";
 
+    const getMovieInfo = async video_id => {
+        const API_KEY = "AIzaSyCdb0haKko0bI2EDidnN-ywbdW1ssnPmB4";
+        const API_URL = `https://www.googleapis.com/youtube/v3/videos?id=${video_id}&key=${API_KEY}&part=snippet,contentDetails,statistics,status`;
+        const info = await axios.get(API_URL);
+        return console.log(info);
+    };
+    getMovieInfo("yd3KYOei8o4");
+
     return (
         <div className={`now-list ${javasc}`}>
             NowList Test
