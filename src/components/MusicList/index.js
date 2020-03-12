c6e1d8af09e279b070f80006a7d7bb1f2b152307
ddock4you <ddock4you@gmail.com>
2020-03-12 23:04:35 +0900

    - gitignore 수정, 음악리스트에서 음악 플레이 중일 때 효과 추가

--- src/components/MusicList/index.js
+++ src/components/MusicList/index.js
@@ -1,20 +1,25 @@
 import React from "react";
 import "./style.scss";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
+// import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Music = ({ musicList }) => {
     const {
         base: { list }
     } = musicList;
+    const playMusic = true;
 
     console.log(list);
     return (
         <div className="music-list">
             <p className="music-list--title">기본목록</p>
             {list.map(music => (
-                <div className="music">
+                <div className="music playing" key={music.id}>
                     <div className="music--jacket">
                         <p>
                             <img src={music.jacket} alt={music.title} />
+                            {playMusic ? <FontAwesomeIcon icon={faPlay} /> : ""}
                         </p>
                     </div>
                     <div className="music--info">
