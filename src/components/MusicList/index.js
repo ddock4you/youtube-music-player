ff35ea2437be7c1ecd2422e7d125dd8b715cbace
ddock4you <ddock4you@gmail.com>
2020-03-29 23:10:06 +0900

    - url 입력창 스타일링 중

--- src/components/MusicList/index.js
+++ src/components/MusicList/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.scss";
 import { faPlay } from "@fortawesome/free-solid-svg-icons";
+import { faTimesCircle } from "@fortawesome/free-regular-svg-icons";
 // import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -11,23 +12,34 @@ const Music = ({ musicList }) => {
     return (
         <div className="music-list">
             <p className="music-list--title">기본목록</p>
-            {musicList.map(music => (
-                <div className="music playing" key={music.id}>
-                    <div className="music--jacket">
-                        <p>
-                            <img src={music.jacket} alt={music.title} />
-                            {playMusic ? <FontAwesomeIcon icon={faPlay} /> : ""}
-                        </p>
-                    </div>
-                    <div className="music--info">
-                        <div className="music--info__title">{music.title}</div>
-                        <div className="music--info__singer">
-                            {music.singer}
+            <div className="music-list--area">
+                {musicList.map(music => (
+                    <div className="music playing" key={music.id}>
+                        <div className="music--jacket">
+                            <p>
+                                <img src={music.jacket} alt={music.title} />
+                                {playMusic ? (
+                                    <FontAwesomeIcon icon={faPlay} />
+                                ) : (
+                                    ""
+                                )}
+                            </p>
+                        </div>
+                        <div className="music--info">
+                            <div className="music--info__title">
+                                {music.title}
+                            </div>
+                            <div className="music--info__singer">
+                                {music.singer}
+                            </div>
+                        </div>
+                        <div className="music--duration">{music.duration}</div>
+                        <div className="music--remove">
+                            <FontAwesomeIcon icon={faTimesCircle} />
                         </div>
                     </div>
-                    <div className="music--duration">{music.duration}</div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     );
 };
