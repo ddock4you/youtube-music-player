01c3a58a3e8961a600d0b17fb37186947f4140ad
ddock4you <ddock4you@gmail.com>
2020-04-01 23:19:55 +0900

    Updates

---
+++ precache-manifest.569377273444d92d711842809d921ae0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23d1a270602759e98f480b47e33dc3ba",
+    "url": "/youtube-music-player/index.html"
+  },
+  {
+    "revision": "4ff4a0e34065e56909f0",
+    "url": "/youtube-music-player/static/css/2.6d7d54d2.chunk.css"
+  },
+  {
+    "revision": "6beb8d1a059a43c5a56b",
+    "url": "/youtube-music-player/static/css/main.3be7b146.chunk.css"
+  },
+  {
+    "revision": "4ff4a0e34065e56909f0",
+    "url": "/youtube-music-player/static/js/2.3b1fcda2.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/youtube-music-player/static/js/2.3b1fcda2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6beb8d1a059a43c5a56b",
+    "url": "/youtube-music-player/static/js/main.72d739f8.chunk.js"
+  },
+  {
+    "revision": "0bc584fa320458196c81",
+    "url": "/youtube-music-player/static/js/runtime-main.3de20c12.js"
+  }
+]);
